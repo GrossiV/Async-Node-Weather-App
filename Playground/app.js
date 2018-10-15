636a4475642591121f8d87003c096a5207615351
Victor Grossi <victoraugustogrossi@gmail.com>
2018-10-14 21:18:13 -0300

    Requisição de api

--- Playground/app.js
+++ Playground/app.js
@@ -4,5 +4,14 @@ request({
     url: 'http://www.mapquestapi.com/geocoding/v1/address?key=5y4lyDIGoKrFX8ogTtoeElTAt87a6OFG&location=1301%20lombard%20street%20philadelphia',
     json: true
 },(error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].providedLocation.location}`)
+    console.log(`lat: ${body.results[0].locations[0].latLng.lat}`)
+    console.log(`lng: ${body.results[0].locations[0].latLng.lng}`)
 });
+
+
+
+//http://www.mapquestapi.com/geocoding/v1/address?key=5y4lyDIGoKrFX8ogTtoeElTAt87a6OFG&location=1301%20lombard%20street%20philadelphia
+
+//  The latitude is stored on the response body here: body.results[0].locations[0].latLng.lat
+//  The longitude is stored on the response body here: body.results[0].locations[0].latLng.lng
--- Playground/callbacks.js
+++ Playground/callbacks.js
@@ -14,7 +14,3 @@ getUser(42, (user) => {
     console.log(user);
 });
 
-//http://www.mapquestapi.com/geocoding/v1/address?key=5y4lyDIGoKrFX8ogTtoeElTAt87a6OFG&location=1301%20lombard%20street%20philadelphia
-
-//  The latitude is stored on the response body here: body.results[0].locations[0].latLng.lat
-//  The longitude is stored on the response body here: body.results[0].locations[0].latLng.lng
